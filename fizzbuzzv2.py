e3406bb4188b6619246cade84d1cf8eeda86a751
yeungpirate <yeungpirate@gmail.com>
2018-01-23 18:33:45 +0000

    Functioning user input.

--- fizzbuzzv2.py
+++ fizzbuzzv2.py
@@ -1,20 +1,23 @@
 import sys
 
-n = 1
-if len(sys.argv) > 1:
-    n = sys.argv[1:]
-
-try:
-    while n <= 100:
-        if n % 5 == 0 and n % 3 == 0:
-            print("fizzbuzz")
-        elif n % 5 == 0:
-            print("buzz")
-        elif n % 3 == 0:
-            print("fizz")
+while True:
+    try:
+        if len(sys.argv) > 1:
+            k = int(sys.argv[1])
         else:
-            print(n)
-        n += 1
+            k = int(input("Please enter the upper limit: "))
+        break
+    except:
+        print("Please enter a number between 1 and 100.")
+n = 1
 
-except:
-    print("Please enter a number between 1 and 100.")
+while n <= k:
+    if n % 5 == 0 and n % 3 == 0:
+        print("fizzbuzz")
+    elif n % 5 == 0:
+        print("buzz")
+    elif n % 3 == 0:
+        print("fizz")
+    else:
+        print(n)
+    n += 1
